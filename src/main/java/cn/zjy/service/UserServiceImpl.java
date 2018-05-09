0b6325013c306409f574492f68a33bc738b313a1
zjy <zjylove2006@126.com>
2018-05-09 09:33:59 +0800

    第一次提交jenkins_springboot

--- src/main/java/cn/zjy/service/UserServiceImpl.java
+++ src/main/java/cn/zjy/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package cn.zjy.service;
 
 import cn.zjy.dao.UserMapper;
 import cn.zjy.pojo.User;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,7 @@ import java.util.List;
 @Service
 @Transactional
 public class UserServiceImpl implements UserService{
+	private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     //注入UserMapper
     @Autowired
@@ -21,4 +24,5 @@ public class UserServiceImpl implements UserService{
         List<User> userList = userMapper.findAll();
         return userList;
     }
+    log.info("查询到的用户列表是:{}",userList);
 }
