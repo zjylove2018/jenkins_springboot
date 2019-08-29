07324590fb0c3e82403aa479c7375a3e062b7f6d
zhoujy <zhoujy@yinhechuangxiang.com>
2019-08-29 15:39:33 +0800

    jenkins整合

--- src/main/java/cn/zjy/controller/HelloController.java
+++ src/main/java/cn/zjy/controller/HelloController.java
@@ -2,18 +2,21 @@ package cn.zjy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@ResponseBody
 public class HelloController {
 
     @RequestMapping("/hello")
     public Map hello(){
-        Map map=new HashMap<>();
+        Map<String, String> map=new HashMap<>();
         map.put("id","123");
         map.put("name","张三");
+        map.put("address","北京");
         return map;
     }
 }
